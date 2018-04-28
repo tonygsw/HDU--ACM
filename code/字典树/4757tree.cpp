d01fd1217a4e0997e70934345464bfd946358372
tony <tonygsw@github.com>
2018-04-28 13:24:21 +0800

    2018.4.28

---
+++ code/字典树/4757tree.cpp
@@ -0,0 +1,23 @@
+/*
+    data:2018.04.26
+    author:gswycf
+    link:http://acm.hdu.edu.cn/showproblem.php?pid=4825
+    accout:tonysave
+*/
+#define ll long long
+#define IO ios::sync_with_stdio(false);
+#define maxn 100005
+
+#include<stdio.h>
+#include<iostream>
+#include<algorithm>
+#include<math.h>
+#include<string.h>
+#include<vector>
+using namespace std;
+int main()
+{
+
+
+}
+
